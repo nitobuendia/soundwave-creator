18f4865052c9c68d66720f0356d2e7cc380bab30
Nito Buendia <effectedcard@gmail.com>
2019-09-21 17:10:08 +0800

    Refactor wave generator to be owner of generating sound samples

--- soundwave.py
+++ soundwave.py
@@ -2,18 +2,27 @@
 
 import time
 import wave_creator
-import wave_functions
+import wave_generator
 import wave_plotter
 
 
 _SOUND_FILE_NAME = 'sounds/sound.wav'
 _SOUND_DURATION_IN_SECONDS = 3
-_SOUND_WAVE_TYPE = wave_functions.SoundWaveType.SIN_WAVE
+_SOUND_WAVE_TYPE = wave_generator.SoundWaveType.SIN_WAVE
+_SOUND_WAVE_OPTIONS = {
+    wave_generator.SoundWaveOption.DEBUG: False,
+}
 _SOUND_WAVE_GRAPH_TYPE = wave_plotter.WaveGraphType.PER_FRAME
 
+
 if __name__ == "__main__":
-  wave_creator.create_sound_wave_file(
-      _SOUND_DURATION_IN_SECONDS, _SOUND_FILE_NAME, _SOUND_WAVE_TYPE)
+  wave_sound_generator = wave_generator.WaveSoundGenerator()
+
+  sound_wave_data = wave_sound_generator.get_wave_sound_samples(
+      _SOUND_DURATION_IN_SECONDS, _SOUND_WAVE_TYPE, _SOUND_WAVE_OPTIONS)
+
+  wave_creator.create_sound_file(
+      _SOUND_FILE_NAME, _SOUND_DURATION_IN_SECONDS, sound_wave_data)
 
   wave_plotter.create_sound_wave_graph(
       _SOUND_FILE_NAME, _SOUND_WAVE_GRAPH_TYPE)
